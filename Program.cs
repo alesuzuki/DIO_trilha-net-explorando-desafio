4c0b237e3c189d5bd5ce3d84d28269554e62109f
Sra Suzuki <alesuzuki@gmail.com>
2024-05-30 14:04:10 -0300

    realizando o desafio de codigo
    realizando o desafio de codigo

--- Program.cs
+++ Program.cs
@@ -6,18 +6,28 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "Alessandra");
+Pessoa p2 = new Pessoa(nome: "Viktor s2");
+Pessoa p3 = new Pessoa(nome: "Felícia");
+Pessoa p4 = new Pessoa(nome: "Leona");
+Pessoa p5 = new Pessoa(nome: "Shiro");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
+hospedes.Add(p4);
+hospedes.Add(p5);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite1 = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 100);
+Suite suite2 = new Suite(tipoSuite: "Boa, mas não premium", capacidade: 5, valorDiaria: 70);
+Suite suite3 = new Suite(tipoSuite: "OK", capacidade: 10, valorDiaria: 50 );
+
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
+
+reserva.CadastrarSuite(suite2);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
